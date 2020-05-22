ccfab52ab067f3e9978fa022e93176ead324c144
jack2015 <jack2015@users.noreply.github.com>
2020-05-22 20:49:35 +0800

    update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup (name = 'enigma2-plugin-extensions-WeatherPlugin2',
        version = '2.1',
        description = 'Plugin to Weather.',
        packages = [pkg],
-       package_dir = {pkg: 'usr'},
+       package_dir = {pkg: 'plugin'},
        package_data = {pkg: ['weather.png', '*/*.png', 'locale/*/LC_MESSAGES/*.mo']},
        cmdclass = setup_translate.cmdclass, # for translation
       )
