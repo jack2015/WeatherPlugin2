0063feac136478cefd373ab3bc4d7e76608dcf32
jack2015 <jack2015@users.noreply.github.com>
2020-05-22 23:31:24 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 import setup_translate
 
-pkg = 'Extensions.WeatherPlugin2'
-setup (name = 'enigma2-plugin-extensions-weatherplugin2',
+pkg = 'Extensions.WeatherPlugin'
+setup (name = 'enigma2-plugin-extensions-weatherplugin',
        version = '2.1',
-       description = 'Weather Plugin 2',
+       description = 'Weather Plugin',
        packages = [pkg],
        package_dir = {pkg: 'plugin'},
        package_data = {pkg: ['weather.png', '*/*.png', 'locale/*/LC_MESSAGES/*.mo']},
